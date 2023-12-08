5cd2e698e9fb6b63f6df2635781561cad673a838
jaypatel200010 <jaypatel200010@gmail.com>
2023-12-08 10:11:16 +0530

    listview

--- assignment7_React_Components_State_Props/listview/src/Components/Listview.js
+++ assignment7_React_Components_State_Props/listview/src/Components/Listview.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 function Car(props) {
   return <li>I am a {props.brand}</li>;
 }
-
+// created function through which list is rendered.
 export default function List() {
   const cars = ["Ford", "BMW", "Audi"];
   return (
@@ -11,6 +11,7 @@ export default function List() {
       <div>
         <h1>The React way to Render a list.</h1>
       </div>
+      {/* passing value of cars to list car. */}
       <ul>
         {cars.map((car) => (
           <Car brand={car} />
@@ -20,4 +21,5 @@ export default function List() {
   );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// rendered a list
 root.render(<List />);
