a7c4439c94edfe7052a5341e7486f91486afbdf5
jaypatel200010 <jaypatel200010@gmail.com>
2023-12-11 09:55:13 +0530

    listview

--- assignment7_React_Components_State_Props/listview/src/App.js
+++ assignment7_React_Components_State_Props/listview/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Incdec from "./Components/Incdec";
 import List from "./Components/Listview";
 
 function App() {
   return (
     <>
       <List></List>
-      <Incdec></Incdec>
     </>
   );
 }
