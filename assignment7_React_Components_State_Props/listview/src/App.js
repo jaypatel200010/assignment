4f0d676c0d78a19bf0aa5802f4fe5c7233800a03
jaypatel200010 <jaypatel200010@gmail.com>
2023-12-11 09:48:02 +0530

    inc_dec

--- assignment7_React_Components_State_Props/listview/src/App.js
+++ assignment7_React_Components_State_Props/listview/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Incdec from "./Components/Incdec";
 import List from "./Components/Listview";
 
 function App() {
   return (
     <>
       <List></List>
+      <Incdec></Incdec>
     </>
   );
 }
